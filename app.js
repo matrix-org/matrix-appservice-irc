f7e1643889e8ec3bbc8ef6c7d29d16d1658dae9b
Kegan Dougal <kegan@matrix.org>
2015-03-17 14:59:18 +0000

    Support running this service as a stand-alone AS.

---
+++ app.js
@@ -0,0 +1,23 @@
+// when invoked with 'node app.js', make an AS with just the IRC service.
+var appservice = require("matrix-appservice");
+var irc = require("./lib/irc-appservice.js");
+
+// load the config file
+var yaml = require("js-yaml");
+var fs = require("fs");
+var config = undefined;
+
+try {
+	config = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8'));
+} 
+catch (e) {
+	console.error(e);
+	return;
+}
+
+irc.configure(config.ircService);
+
+config.appService.service = irc;
+appservice.registerServices([config.appService]);
+
+appservice.runForever();
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
+// When used as a node dep, just export the service.
 var irc = require("./lib/irc-appservice.js");
 module.exports = irc;
