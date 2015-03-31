1c2dabf5a8e817b80bcd7342fde8caafe6020931
Kegan Dougal <kegan@matrix.org>
2015-03-31 15:57:48 +0100

    Add --generate-registration flag

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var irc = require("./lib/irc-appservice.js");
 var yaml = require("js-yaml");
 var fs = require("fs");
 var config = undefined;
+var generateRegistration = process.argv[2] == "--generate-registration";
 
 try {
     config = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8'));
@@ -14,10 +15,27 @@ catch (e) {
     console.error(e);
     return;
 }
-
 irc.configure(config.ircService);
 
 config.appService.service = irc;
+config.appService.shouldStartup = !generateRegistration;
 appservice.registerServices([config.appService]);
 
-appservice.runForever();
+if (generateRegistration) {
+    var fname = "appservice-registration-irc.yaml";
+    console.log("Generating registration file to %s...", fname);
+    appservice.getRegistrations().done(function(entries) {
+        var registration = entries[0];
+        fs.writeFile(fname, yaml.safeDump(registration), function(e) {
+            if (e) {
+                console.error("Failed to write registration file: %s", e);
+                return;
+            }
+            console.log("Generated registration file located at: %s", fname);
+            process.exit(code=0);
+        });
+    });
+}
+else {
+    appservice.runForever();
+}
