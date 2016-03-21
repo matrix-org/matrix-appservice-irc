f132505b39cc723733b79cbfdba4c991d265a8ae
Kegan Dougal <kegan@matrix.org>
2016-03-21 18:00:48 +0000

    Mostly complete upgrade script

--- scripts/upgrade-db-0.1-to-0.2.js
+++ scripts/upgrade-db-0.1-to-0.2.js
@@ -2,6 +2,8 @@
 "use strict";
 
 var Promise = require("bluebird");
+var Datastore = require("nedb");
+Promise.promisifyAll(Datastore.prototype);
 var nopt = require("nopt");
 var path = require("path");
 var fs = require("fs");
@@ -9,13 +11,14 @@ var fs = require("fs");
 var opts = nopt({
     "help": Boolean,
     rooms: path,
-    users: path
+    users: path,
+    clients: path
 }, {
     "h": "--help"
 });
 
-if (!opts.help && (!opts.rooms || !opts.users)) {
-    console.log("--rooms and --users are required.");
+if (!opts.help && (!opts.rooms || !opts.users || !opts.clients)) {
+    console.log("--rooms, --clients and --users are required.");
     opts.help = true;
 }
 
@@ -29,18 +32,116 @@ will be dumped to a directory called "0.2-db" in the current working directory.
 
  Usage:
    --rooms   The path to rooms.db. Required.
-   --users   The path to users.db. Required.`
+   --users   The path to users.db. Required.
+   --clients The path to irc_clients.db. Required.`
 );
 process.exit(0);
 }
 
 
-var upgradeUsers = Promise.coroutine(function*(dbPath) {
-    console.log("Upgrading users database: %s", dbPath);
+var upgradeUsers = Promise.coroutine(function*(usersDb, clientDb) {
+    console.log("Upgrading users database");
+    // 0.1 users.db => user_id, localpart, display_name, set_display_name
+    // 0.1 irc_clients.db => user_id, domain, nick, username
+    var users = yield usersDb.findAsync({});
+    var clients = yield clientDb.findAsync({});
+
+    var userMap = {};
+    users.forEach((user) => {
+        userMap[user.user_id] = user;
+    });
+    clients.forEach((client) => {
+        var user = userMap[client.user_id];
+        if (!user) {
+            userMap[client.user_id] = {
+                user_id: client.user_id
+            };
+            user = userMap[client.user_id];
+        }
+        if (user.client) {
+            console.warn("Found multiple clients for %s - dropping one.", client.user_id);
+        }
+        user.client = client;
+    });
+    // 0.2 users.db => type=matrix, id=<user_id>
+    //                 data={
+    //                   client_config: { "irc_network": {nick=Nick, username=Username} }
+    //                   localpart: <localpart>
+    //                 }
+    var newUserStore = new Datastore({
+        filename: "0.2-db/users.db",
+        autoload: true
+    });
+    var insertions = Object.keys(userMap).map((userId) => {
+        var user = userMap[userId];
+        var config = undefined;
+        if (user.client) {
+            config = {
+                [user.client.domain.replace(/\./g, "_")]: {
+                    nick: user.client.nick,
+                    username: user.client.username
+                }
+            }
+        }
+        return {
+            type: "matrix",
+            id: userId,
+            data: {
+                localpart: user.localpart,
+                client_config: config
+            }
+        };
+    });
+
+    yield newUserStore.insertAsync(insertions);
 });
 
-var upgradeRooms = Promise.coroutine(function*(dbPath) {
-    console.log("Upgrading rooms database: %s", dbPath);
+var upgradeRooms = Promise.coroutine(function*(db) {
+    console.log("Upgrading rooms database");
+    // 0.1 rooms.db =>
+    // room_id, irc_addr(domain), irc_chan, from_config, type(channel)      CHANNELS
+    // user_id, type(admin), room_id                                        ADMIN
+    // real_user_id, virtual_user_id, room_id, irc_addr, irc_chan, type(pm) PM
+    var rooms = yield db.findAsync({});
+
+    // 0.2 rooms.db =>
+    // CHANNELS
+    // type=matrix, id=<room_id>  data={extras:{}}
+    // type=remote, id=<domain_@_chan> data={domain,channel,type(channel)}
+    // type=union, link_key=<room_id remote_id>, remote_id, matrix_id, data:{from_config}
+    //
+    // ADMIN
+    // type=matrix, id=<room_id> data={extras:{admin_id:<user_id>}}
+    //
+    // PM
+    // type=remote, id=<domain_@_nick> data={domain,channel,type(pm)}
+    // type=union, link_key="PM real_user_id virt_user_id", remote_id, matrix_id,
+    //                                                      data={real_user_id, virtual_user_id}
+    var newRoomStore = new Datastore({
+        filename: "0.2-db/rooms.db",
+        autoload: true
+    });
+    var insertions = [];
+    rooms.forEach((room) => {
+        switch (room.type) {
+            case "pm":
+                break;
+            case "channel":
+                break;
+            case "admin":
+                insertions.push({
+                    type: "matrix", id: room.room_id, data: {
+                        extras: {
+                            admin_id: room.user_id
+                        }
+                    }
+                });
+                break;
+            default:
+                throw new Error("Unknown room type " + room.type);
+        }
+    });
+    yield newRoomStore.insertAsync(insertions);
 });
 
 Promise.coroutine(function*() {
@@ -50,7 +151,19 @@ Promise.coroutine(function*() {
     catch (err) {
         if (err.code !== "EEXIST") { throw err; }
     }
-    yield upgradeUsers(opts.users);
-    yield upgradeRooms(opts.rooms);
+    var userStore = new Datastore({
+        filename: opts.users,
+        autoload: true
+    });
+    var clientStore = new Datastore({
+        filename: opts.clients,
+        autoload: true
+    });
+    yield upgradeUsers(userStore, clientStore);
+    var roomStore = new Datastore({
+        filename: opts.rooms,
+        autoload: true
+    });
+    yield upgradeRooms(roomStore);
     console.log("Upgrade complete.");
 })();
