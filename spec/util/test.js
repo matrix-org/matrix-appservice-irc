7fbb92637011a118d3b6ac040975769828d03f87
Luke Barnard <lukeb@openmarket.com>
2016-09-29 17:08:21 +0100

    Attempt to fix problem with bot clients joining forever and never dying after tests.

--- spec/util/test.js
+++ spec/util/test.js
@@ -36,11 +36,14 @@ module.exports.mkEnv = function() {
 module.exports.initEnv = function(env, customConfig) {
     // wipe the database entirely then call configure and register on the IRC
     // service.
+
     return env.dbHelper._reset(env.config.ircService.databaseUri).then(function() {
-        return env.main.runBridge(
-            env.config._port, customConfig || env.config,
-            AppServiceRegistration.fromObject(env.config._registration)
-        );
+        return env.main.killBridge().then(function() {
+            return env.main.runBridge(
+                env.config._port, customConfig || env.config,
+                AppServiceRegistration.fromObject(env.config._registration)
+            );
+        });
     }).catch(function(e) {
         var msg = JSON.stringify(e);
         if (e.stack) {
