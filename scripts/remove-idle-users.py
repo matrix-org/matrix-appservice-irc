896de7bd051e3f2005f2e4295f71df97886d0ee2
Luke Barnard <lukeb@openmarket.com>
2017-01-09 18:32:32 +0000

    Use joined_members endpoint, use user_prefix, add a count to show successes, add summary of failures

--- scripts/remove-idle-users.py
+++ scripts/remove-idle-users.py
@@ -5,11 +5,6 @@ import sys
 import json
 import urllib
 import requests
-import re
-
-## debug request
-import httplib as http_client
-http_client.HTTPConnection.debuglevel = 1
 
 def get_room_id(homeserver, alias, token):
     res = requests.get(homeserver + "/_matrix/client/r0/directory/room/" + urllib.quote(alias) + "?access_token=" + token)
@@ -24,22 +19,23 @@ def is_idle(homeserver, user_id, token, activity_threshold_ms):
     return get_last_active_ago(homeserver, user_id, token) > activity_threshold_ms
 
 def get_idle_users(homeserver, room_id, token, since):
-    res = requests.get(homeserver + "/_matrix/client/r0/rooms/" + urllib.quote(room_id) + "/members?access_token=" + token)
-    user_ids = [event["state_key"] for event in res.json().get("chunk", None)]
+    res = requests.get(homeserver + "/_matrix/client/r0/rooms/" + urllib.quote(room_id) + "/joined_members?access_token=" + token)
+    user_ids = [user_id for user_id in res.json().get("joined", None)]
 
     activity_threshold_ms = since * 24 * 60 * 60 * 1000
 
     return [user_id for user_id in user_ids if is_idle(homeserver, user_id, token, activity_threshold_ms)]
 
-def kick_idlers(homeserver, homeserver_domain, room_id, token, since, user_template=None):
+def kick_idlers(homeserver, homeserver_domain, room_id, token, since, user_prefix):
     reason = "Being idle for >%s days" % since
 
     user_ids = get_idle_users(homeserver, room_id, token, since)
     failure_responses = []
-    print("Kicking %s idle users from %s" % (len(user_ids), room_id))
+    count = 0
+    print("There are %s idle users in %s" % (len(user_ids), room_id))
     for user_id in user_ids:
-        # Ignore unclaimed users, if user_template is specified
-        if user_template and not claims_user_id(user_id, user_template, homeserver_domain):
+        # Ignore users that do not start with the user_prefix
+        if not user_id.startswith(user_prefix):
             continue
         res = requests.post(
             homeserver + "/_matrix/client/r0/rooms/" + urllib.quote(room_id) + "/kick?access_token=" + token,
@@ -50,59 +46,23 @@ def kick_idlers(homeserver, homeserver_domain, room_id, token, since, user_templ
         )
         if res.status_code >= 400:
             failure_responses.append({ "user_id": user_id, "response_json": res.json()})
+        else:
+            count += 1
+    print("Kicked %s users in total" % count)
 
+    if len(failure_responses) == 0:
+        return
     print("Could not kick the following users:")
     for failure in failure_responses:
-        print("%s : %s - %s", failure.user_id, failure.response_json['error'], failure.response_json['errcode'])
+        print("%s : %s - %s" % (failure["user_id"], failure["response_json"]["error"], failure["response_json"]["errcode"]))
 
-
-def claims_user_id(user_id, user_template, homeserver_domain):
-    # the server claims the given user ID if the ID matches the user ID template.
-    regex = template_to_regex(
-        user_template,
-        {
-            "$SERVER": homeserver_domain
-        },
-        {
-            "$NICK": "(.*)"
-        },
-        escapeRegExp(":" + homeserver_domain)
-    )
-    print("Matching %s to %s" % (regex, user_id))
-    return re.match(regex, user_id)
-
-def template_to_regex(template, literal_vars, regex_vars, suffix = ""):
-    # The 'template' is a literal string with some special variables which need
-    # to be find/replaced.
-    regex = template;
-    for k in literal_vars:
-        regex = re.sub(escapeRegExp(k), regex, literal_vars[k])
-
-    # at this point the template is still a literal string, so escape it before
-    # applying the regex vars.
-    regex = escapeRegExp(regex);
-    # apply regex vars
-
-    for k in regex_vars:
-        regex = re.sub(
-            # double escape, because we bluntly escaped the entire string before
-            # so our match is now escaped.
-            escapeRegExp(escapeRegExp(k)), regex, regex_vars[k]
-        )
-
-    return regex + suffix
-
-def escapeRegExp(s):
-    return re.escape(s);
-
-
-def main(token, alias, homeserver, homeserver_domain, since, user_template):
+def main(token, alias, homeserver, homeserver_domain, since, user_prefix):
     print("Removing idle users in %s" % alias)
     room_id = get_room_id(homeserver, alias, token)
     if not room_id:
         raise Exception("Cannot resolve room alias to room_id")
 
-    kick_idlers(homeserver, homeserver_domain, room_id, token, since, user_template)
+    kick_idlers(homeserver, homeserver_domain, room_id, token, since, user_prefix)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Remove idle users from a given Matrix room")
@@ -111,9 +71,9 @@ if __name__ == "__main__":
     parser.add_argument("-u", "--homeserver", help="Base homeserver URL eg 'https://matrix.org'", required=True)
     parser.add_argument("-d", "--domain", help="Domain of the homserver eg 'matrix.org'", required=True)
     parser.add_argument("-s", "--since", type=int, help="Days since idle users have been offline for eg '30'", required=True)
-    parser.add_argument("-e", "--template", help="User template to determine whether a user should be kicked. E.g. @$SERVER_$NICK", required=True)
+    parser.add_argument("-p", "--prefix", help="User prefix to determine whether a user should be kicked. E.g. @freenode_", required=True)
     args = parser.parse_args()
     if not args.token or not args.alias or not args.homeserver:
         parser.print_help()
         sys.exit(1)
-    main(token=args.token, alias=args.alias, homeserver=args.homeserver, homeserver_domain=args.domain, since=args.since, user_template=args.template)
+    main(token=args.token, alias=args.alias, homeserver=args.homeserver, homeserver_domain=args.domain, since=args.since, user_prefix=args.prefix)
