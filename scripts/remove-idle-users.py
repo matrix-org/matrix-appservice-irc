f8ec915f42df165306b44adf1db8a6bbcd4e6f76
Luke Barnard <lukeb@openmarket.com>
2017-01-10 09:57:24 +0000

    Code review response:
     - Removed homeserver_domain as it is not used
     - Use membership state API, as Synapse doesn't pass the reason on /kick
     - Handle non JSON body error responses
     - Improved logging

--- scripts/remove-idle-users.py
+++ scripts/remove-idle-users.py
@@ -26,7 +26,7 @@ def get_idle_users(homeserver, room_id, token, since):
 
     return [user_id for user_id in user_ids if is_idle(homeserver, user_id, token, activity_threshold_ms)]
 
-def kick_idlers(homeserver, homeserver_domain, room_id, token, since, user_prefix):
+def kick_idlers(homeserver, room_id, token, since, user_prefix):
     reason = "Being idle for >%s days" % since
 
     user_ids = get_idle_users(homeserver, room_id, token, since)
@@ -37,43 +37,49 @@ def kick_idlers(homeserver, homeserver_domain, room_id, token, since, user_prefi
         # Ignore users that do not start with the user_prefix
         if not user_id.startswith(user_prefix):
             continue
-        res = requests.post(
-            homeserver + "/_matrix/client/r0/rooms/" + urllib.quote(room_id) + "/kick?access_token=" + token,
+        res = requests.put(
+            homeserver + "/_matrix/client/r0/rooms/" +
+            urllib.quote(room_id) + "/state/m.room.member/" +
+            urllib.quote(user_id) + "?access_token=" + token,
             data = json.dumps({
                 "reason": reason,
-                "user_id": user_id
+                "membership": "leave"
             })
         )
         if res.status_code >= 400:
-            failure_responses.append({ "user_id": user_id, "response_json": res.json()})
+            failure = { "user_id": user_id }
+            try:
+                failure["response_json"] = res.json()
+            except Exception as e:
+                print("Could not get JSON body from failure response: %s" % e)
+            failure_responses.append()
         else:
             count += 1
-    print("Kicked %s users in total" % count)
+    print("Kicked %s/%s users in total (%s failed requests)" % (count, len(user_ids), len(failure_responses)))
 
     if len(failure_responses) == 0:
         return
     print("Could not kick the following users:")
     for failure in failure_responses:
-        print("%s : %s - %s" % (failure["user_id"], failure["response_json"]["error"], failure["response_json"]["errcode"]))
+        print("%s : %s - %s" % (failure["user_id"], failure["response_json"]))
 
-def main(token, alias, homeserver, homeserver_domain, since, user_prefix):
+def main(token, alias, homeserver, since, user_prefix):
     print("Removing idle users in %s" % alias)
     room_id = get_room_id(homeserver, alias, token)
     if not room_id:
         raise Exception("Cannot resolve room alias to room_id")
 
-    kick_idlers(homeserver, homeserver_domain, room_id, token, since, user_prefix)
+    kick_idlers(homeserver, room_id, token, since, user_prefix)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Remove idle users from a given Matrix room")
     parser.add_argument("-t", "--token", help="The access token", required=True)
     parser.add_argument("-a", "--alias", help="The alias of the room eg '#freenode_#matrix-dev:matrix.org'", required=True)
     parser.add_argument("-u", "--homeserver", help="Base homeserver URL eg 'https://matrix.org'", required=True)
-    parser.add_argument("-d", "--domain", help="Domain of the homserver eg 'matrix.org'", required=True)
     parser.add_argument("-s", "--since", type=int, help="Days since idle users have been offline for eg '30'", required=True)
     parser.add_argument("-p", "--prefix", help="User prefix to determine whether a user should be kicked. E.g. @freenode_", required=True)
     args = parser.parse_args()
     if not args.token or not args.alias or not args.homeserver:
         parser.print_help()
         sys.exit(1)
-    main(token=args.token, alias=args.alias, homeserver=args.homeserver, homeserver_domain=args.domain, since=args.since, user_prefix=args.prefix)
+    main(token=args.token, alias=args.alias, homeserver=args.homeserver, since=args.since, user_prefix=args.prefix)
