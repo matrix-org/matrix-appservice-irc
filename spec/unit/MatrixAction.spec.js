1fd67c5b7c2b6aecf3cd657b12f4f0f7cd7a7a31
Will Hunt <will@half-shot.uk>
2018-08-15 17:52:39 +0100

    Test text too

--- spec/unit/MatrixAction.spec.js
+++ spec/unit/MatrixAction.spec.js
@@ -8,12 +8,14 @@ describe("MatrixAction", function() {
             "Some Person": "@foobar:localhost"
         });
         expect(action.text).toEqual("Some text");
+        expect(action.text).toEqual("Some text");
     });
     it("should highlight a user", () => {
         let action = new MatrixAction("message", "JCDenton, it's a bomb!", "JCDenton, it's a bomb!", null);
         action.formatMentions({
             "JCDenton": "@jc.denton:unatco.gov"
         });
+        expect(action.text).toEqual("JCDenton, it's a bomb!");
         expect(action.htmlText).toEqual("<a href=\"https://matrix.to/#/@jc.denton:unatco.gov\">JCDenton</a>, it's a bomb!");
     });
     it("should highlight a user, regardless of case", () => {
@@ -21,6 +23,7 @@ describe("MatrixAction", function() {
         action.formatMentions({
             "jcdenton": "@jc.denton:unatco.gov"
         });
+        expect(action.text).toEqual("JCDenton, it's a bomb!");
         expect(action.htmlText).toEqual("<a href=\"https://matrix.to/#/@jc.denton:unatco.gov\">jcdenton</a>, it's a bomb!");
     });
     it("should highlight a user, with plain text", () => {
@@ -28,6 +31,7 @@ describe("MatrixAction", function() {
         action.formatMentions({
             "JCDenton": "@jc.denton:unatco.gov"
         });
+        expect(action.text).toEqual("JCDenton, it's a bomb!");
         expect(action.htmlText).toEqual("<a href=\"https://matrix.to/#/@jc.denton:unatco.gov\">JCDenton</a>, it's a bomb!");
     });
     it("should highlight a user, with weird characters", () => {
@@ -36,6 +40,7 @@ describe("MatrixAction", function() {
             "JCDenton[m]": "@jc.denton:unatco.gov"
         });
         expect(action.htmlText).toEqual("<a href=\"https://matrix.to/#/@jc.denton:unatco.gov\">JCDenton[m]</a>, it's a bomb!");
+        expect(action.text).toEqual("`||JCDenton[m], it's a bomb!");
     });
     it("should highlight multiple users", () => {
         let action = new MatrixAction("message", "JCDenton is sent to assassinate PaulDenton", "JCDenton is sent to assassinate PaulDenton", null);
@@ -43,6 +48,7 @@ describe("MatrixAction", function() {
             "JCDenton": "@jc.denton:unatco.gov",
             "PaulDenton": "@paul.denton:unatco.gov"
         });
+        expect(action.text).toEqual("JCDenton is sent to assassinate PaulDenton");
         expect(action.htmlText).toEqual(
             "<a href=\"https://matrix.to/#/@jc.denton:unatco.gov\">JCDenton</a> is sent" +
             " to assassinate <a href=\"https://matrix.to/#/@paul.denton:unatco.gov\">PaulDenton</a>");
@@ -52,6 +58,7 @@ describe("MatrixAction", function() {
         action.formatMentions({
             "JCDenton": "@jc.denton:unatco.gov"
         });
+        expect(action.text).toEqual("JCDenton, meet JCDenton");
         expect(action.htmlText).toEqual("<a href=\"https://matrix.to/#/@jc.denton:unatco.gov\">JCDenton</a>, meet <a href=\"https://matrix.to/#/@jc.denton:unatco.gov\">JCDenton</a>");
     });
 });
