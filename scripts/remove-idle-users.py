5bc4e8075401654c605ba8acbf0e3197f281346f
Ruben Barkow <rubo77@users.noreply.github.com>
2019-01-26 03:19:05 +0100

    repair prefix selection and add simulate and verbose option
    The function `should_ignore_user() ignored all users **with** that user_prefix instead of all users **without**! This is fixed now.
    
    Also I added some more validation to give the right error message, if an argument is wrong.

--- scripts/remove-idle-users.py
+++ scripts/remove-idle-users.py
@@ -21,7 +21,7 @@ def is_idle(homeserver, user_id, token, activity_threshold_ms):
     return get_last_active_ago(homeserver, user_id, token) > activity_threshold_ms
 
 def should_ignore_user(test_user_id, user_prefix, bot_user_id):
-    return test_user_id.startswith(user_prefix) or test_user_id == bot_user_id
+    return not test_user_id.startswith(user_prefix) or test_user_id == bot_user_id
 
 def get_idle_users(homeserver, room_id, token, since, user_prefix, bot_user_id):
     res = requests.get(homeserver + "/_matrix/client/r0/rooms/" + urllib.quote(room_id) + "/joined_members?access_token=" + token)
@@ -38,6 +38,7 @@ def get_idle_users(homeserver, room_id, token, since, user_prefix, bot_user_id):
     return [user_id for user_id in user_ids if is_idle(homeserver, user_id, token, activity_threshold_ms)]
 
 def kick_idlers(homeserver, room_id, token, since, user_prefix, bot_user_id):
+    global args
     print("%s : Processing %s" % (str(datetime.now()), room_id))
     reason = "Being idle for >%s days" % since
 
@@ -46,24 +47,29 @@ def kick_idlers(homeserver, room_id, token, since, user_prefix, bot_user_id):
     count = 0
     print("%s :   %s idle users in %s" % (str(datetime.now()), len(user_ids), room_id))
     for user_id in user_ids:
-        res = requests.put(
-            homeserver + "/_matrix/client/r0/rooms/" +
-            urllib.quote(room_id) + "/state/m.room.member/" +
-            urllib.quote(user_id) + "?access_token=" + token,
-            data = json.dumps({
-                "reason": reason,
-                "membership": "leave"
-            })
-        )
-        if res.status_code >= 400:
-            failure = { "user_id" : user_id }
-            try:
-                failure["response_json"] = res.json()
-            except Exception as e:
-                print("Could not get JSON body from failure response: %s" % e)
-            failure_responses.append(failure)
-        else:
+        if args.verbose or args.simulate:
+            print("kick user '%s'" % user_id)
+        if args.simulate:
             count += 1
+        else:
+            res = requests.put(
+                homeserver + "/_matrix/client/r0/rooms/" +
+                urllib.quote(room_id) + "/state/m.room.member/" +
+                urllib.quote(user_id) + "?access_token=" + token,
+                data = json.dumps({
+                    "reason": reason,
+                    "membership": "leave"
+                })
+            )
+            if res.status_code >= 400:
+                failure = { "user_id" : user_id }
+                try:
+                    failure["response_json"] = res.json()
+                except Exception as e:
+                    print("Could not get JSON body from failure response: %s" % e)
+                failure_responses.append(failure)
+            else:
+                count += 1
     if count > 0:
         print("%s :   %s/%s kicked users in total (%s failed requests)" % (str(datetime.now()), count, count + len(failure_responses), len(failure_responses)))
 
@@ -75,7 +81,7 @@ def kick_idlers(homeserver, room_id, token, since, user_prefix, bot_user_id):
 
 def main(token, alias, homeserver, since, user_prefix, user_id, room_id=None):
     if room_id is None:
-        print("Removing idle users in %s" % alias)
+        print("Removing idle users in %s, starting with prefix '%s'" % (alias, user_prefix))
         room_id = get_room_id(homeserver, alias, token)
 
     if not room_id:
@@ -92,6 +98,8 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--since", type=int, help="Days since idle users have been offline for eg '30'", required=True)
     parser.add_argument("-p", "--prefix", help="User prefix to determine whether a user should be kicked. E.g. @freenode_", required=True)
     parser.add_argument("-u", "--user", help="The user ID of the AS bot. E.g '@appservice-irc:matrix.org'", required=True)
+    parser.add_argument('-n', '--simulate', action="count", help="simulate only, see what would happen")
+    parser.add_argument('-v', '--verbose', action="count", help="increase output verbosity")
     args = parser.parse_args()
     if not args.token or not args.homeserver or not args.user or (not args.alias and not args.room):
         parser.print_help()
@@ -99,5 +107,17 @@ if __name__ == "__main__":
     if args.user[0] != "@":
         parser.print_help()
         print("--user must start with '@'")
+        sys.exit(1)
+    if args.prefix[0] != "@":
+        parser.print_help()
+        print("--prefix must start with '@'")
+        sys.exit(1)
+    if args.room and args.room[0] != "!":
+        parser.print_help()
+        print("--room must start with '!'")
+        sys.exit(1)
+    if args.alias and args.alias[0] != "#":
+        parser.print_help()
+        print("--alias must start with '#'")
         sys.exit(1)
     main(token=args.token, alias=args.alias, homeserver=args.homeserver, since=args.since, user_prefix=args.prefix, room_id=args.room, user_id=args.user)
