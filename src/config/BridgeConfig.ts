62deefb00a294bc1d98e132aeef5ff958a685fd8
Will Hunt <will@half-shot.uk>
2019-10-10 11:32:29 +0100

    Create an interface type for BridgeConfig

---
+++ src/config/BridgeConfig.ts
@@ -0,0 +1,54 @@
+import { IrcServerConfig } from "../irc/IrcServer";
+import { LoggerConfig } from "../logging";
+
+export interface BridgeConfig {
+    matrixHandler: {
+
+    };
+    ircHandler: {
+
+    };
+    database: {
+        engine: string;
+        connectionString: string;
+    };
+    homeserver: {
+        url: string;
+        domain: string;
+        enablePresence?: boolean;
+        dropMatrixMessagesAfterSecs?: number;
+    };
+    ircService: {
+        servers: {[domain: string]: IrcServerConfig};
+        provisioning: {
+            enabled: boolean;
+            requestTimeoutSeconds: number;
+            ruleFile: string;
+            enableReload: boolean;
+        };
+        logging: LoggerConfig;
+        debugApi: {
+            enabled: boolean;
+            port: number;
+        };
+        /** @deprecated Use `BridgeConfig.database` */
+        databaseUri?: string;
+        metrics: {
+            enabled: boolean;
+            remoteUserAgeBuckets: string[];
+        };
+        passwordEncryptionKeyPath?: string;
+        ident: {
+            enabled: boolean;
+            address: string;
+            port: number;
+        };
+        statsd: {
+            hostname: string;
+            port: number;
+        };
+    };
+    advanced: {
+        maxHttpSockets: number;
+    };
+}
