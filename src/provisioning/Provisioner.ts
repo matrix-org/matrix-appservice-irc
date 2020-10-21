2d73f6ebca7bceed41134f761628f12dc4b8f707
Will Hunt <will@half-shot.uk>
2020-10-21 18:06:39 +0100

    linting

--- src/provisioning/Provisioner.ts
+++ src/provisioning/Provisioner.ts
@@ -937,7 +937,7 @@ export class Provisioner {
         try {
             await Promise.all([
                 this.partUnlinkedIrcClients(req, roomId, server, ircChannel),
-                this.leaveMatrixVirtuals(req, roomId, server, ircChannel)
+                this.leaveMatrixVirtuals(req, roomId, server)
             ]);
         }
         catch (err) {
@@ -1033,7 +1033,7 @@ export class Provisioner {
         );
     }
 
-    private async leaveMatrixVirtuals(req: ProvisionRequest, roomId: string, server: IrcServer, ircChannel: string) {
+    private async leaveMatrixVirtuals(req: ProvisionRequest, roomId: string, server: IrcServer) {
         const asBot = this.ircBridge.getAppServiceBridge().getBot();
         const roomChannels = await this.ircBridge.getStore().getIrcChannelsForRoomId(
             roomId
