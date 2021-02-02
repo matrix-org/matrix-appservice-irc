902ba132a6fc9af3887f077554f14c3529601443
Ville Ranki <ville.ranki@iki.fi>
2021-02-02 20:33:33 +0200

    Changes requested in PR

--- src/bridge/IrcBridge.ts
+++ src/bridge/IrcBridge.ts
@@ -1197,16 +1197,13 @@ export class IrcBridge {
         await client.leaveChannel(ircRoom.channel);
     }
 
-    public sendIrcAction(ircRoom: IrcRoom, bridgedClient: BridgedClient, action: IrcAction) {
-        log.info(
-            "Sending IRC message in %s as %s (connected=%s)",
-            ircRoom.channel, bridgedClient.nick, Boolean(bridgedClient.status === BridgedClientStatus.CONNECTED)
-        );
+    public async sendIrcAction(ircRoom: IrcRoom, bridgedClient: BridgedClient, action: IrcAction) {
         if (action.text.startsWith("!irc nick ")) {
             const newNick = action.text.substr("!irc nick ".length)
             log.info("Matrix user wants to change nick from %s to %s", bridgedClient.nick, newNick)
             try {
-               return bridgedClient.changeNick(newNick, true)
+                // TODO: Send confirmation on success or failure
+                return await bridgedClient.changeNick(newNick, true)
             }
             catch (err) {
                 if (err.stack) {
@@ -1214,6 +1211,10 @@ export class IrcBridge {
                 }
             }
         }
+        log.info(
+            "Sending IRC message in %s as %s (connected=%s)",
+            ircRoom.channel, bridgedClient.nick, Boolean(bridgedClient.status === BridgedClientStatus.CONNECTED)
+        );
         return bridgedClient.sendAction(ircRoom, action);
     }
 
