eec46e2cb09388e2160ed688e2d609eb23050b05
jesopo <github@lolnerd.net>
2022-11-10 13:53:33 +0000

    errant speech mark in schema/v9.ts

--- src/datastore/postgres/schema/v9.ts
+++ src/datastore/postgres/schema/v9.ts
@@ -4,7 +4,7 @@ export async function runSchema(connection: PoolClient) {
     await connection.query(`
         ALTER TABLE last_seen ADD COLUMN first BIGINT;
         ALTER TABLE last_seen RENAME COLUMN ts TO last;
-        UPDATE last_seen SET first = last";
+        UPDATE last_seen SET first = last;
         ALTER TABLE last_seen ALTER COLUMN first SET NOT NULL;
     `);
 }
