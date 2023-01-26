bddc5016ab025077890287bf5f63a3b0e8d31bc3
Justin Carlson <justinc@element.io>
2023-01-26 11:10:09 -0500

    Fix import error

--- widget/src/index.tsx
+++ widget/src/index.tsx
@@ -6,6 +6,6 @@ import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <App/>
     </React.StrictMode>,
 );
--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    root: 'widget',
+    root: './widget',
     base: '',
     build: {
         outDir: '../public',
