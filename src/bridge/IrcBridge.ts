50b6e9ebe24c66cba80c39cfe140a119573439af
Half-Shot <will@half-shot.uk>
2023-04-26 11:55:21 +0100

    Remove await

--- src/bridge/IrcBridge.ts
+++ src/bridge/IrcBridge.ts
@@ -931,7 +931,7 @@ export class IrcBridge {
         this.bridgeState = "killed";
         log.info("Killing all clients");
         if (!this.config.connectionPool?.persistConnectionsOnShutdown) {
-            await this.clientPool.killAllClients(reason);
+            this.clientPool.killAllClients(reason);
         }
         else {
             log.info(`Persisting connections on shutdown`);
