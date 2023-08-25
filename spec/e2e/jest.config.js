822909351846badbc7512d61f8ed1ddb1a16c2a2
Half-Shot <will@half-shot.uk>
2023-08-25 19:04:46 +0100

    Better reporting for GHA

--- spec/e2e/jest.config.js
+++ spec/e2e/jest.config.js
@@ -1,9 +1,11 @@
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
 /** @type {import('ts-jest/dist/types').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   testTimeout: 60000,
+  reporters: isGithubActions ? [['github-actions', {silent: false}], 'summary'] : ['default'],
   transform: {
     // Use the root tsconfig.json
     // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig/#examples
